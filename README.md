a216e112b951c90c81a92698960a5228fe862f64
Yusen Zheng <ysyszheng@gmail.com>
2022-05-20 09:37:03 +0800

    final update

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 SJTU-NIS2331 Computer Composition and System Architecture course lab: ARM assembly implementation of matrix multiplication.
 
-Solution file: [matrix.s](matrix.s)
+Solution file: [`matrix.s`](./matrix.s)
 
-I hate Keil uVision5, it's stupid and ugly.
+**Usage:**
+
+```zsh
+$> ./generate.sh
+```
+
+If an error occurs at runtime, you should install the relevant dependency packages, the required ones can be found in the [`Dockerfile`](./Dockerfile).
+
+**Output:**
+
+```zsh
+[+] Test 1 succeed.
+[+] Test 2 succeed.
+[+] Test 2 succeed.
+[*] Final score is 30.
+```
+
+I hate Keil uVision5, it's **stupid and ugly**.
--- matrix.s
+++ matrix.s
@@ -9,10 +9,12 @@ matrix_mul
 
 	LDR r3, [r1, #4]	; source1->columns
 	LDR r4, [r2]	; source2->rows
+	PUSH {r3-r4}
 	CMP r3, r4
 	BEQ success
 	MOV r0, #1 ; fail
-	B return
+	B finish
+; ------------------------------------------------------------------------------------------------
 
 success
 	LDR r3, [r1]	; source1->rows
@@ -20,25 +22,27 @@ success
 	LDR r5, [r2]	; source1->columns == source2->rows
 	STR r3, [r0]	; results->rows
 	STR r4, [r0, #4]	; results->columns
-	LDR r0, [r0, #8]	; 0->data
-	LDR r1, [r1, #8]	; 1->data
-	LDR r2, [r2, #8]	; 2->data
+	LDR r0, [r0, #8]	; result_data_address
+	LDR r1, [r1, #8]	; source1->data_address
+	LDR r2, [r2, #8]	; source2->data_address
 	MOV r6, #0	; i
 	MOV r7, #0	; j
 	MOV r8, #0	; k
 	MOV r9, #0	; mul_data1
 	MOV r10, #0	;	mul_data2
 	B loop1
+; ------------------------------------------------------------------------------------------------
 
 loop1
 	CMP r6, r3	; i < source1->rows
-	BEQ return
+	BEQ finish
 	PUSH {LR}
 	BL loop2
 	POP {LR}
 	ADD r6, #1	; i++
 	MOV r7, #0	; j = 0
 	B loop1
+; ------------------------------------------------------------------------------------------------
 
 loop2
 	CMP r7, r4	; j < source2->columns
@@ -49,18 +53,19 @@ loop2
 	ADD r7, #1	; j++
 	MOV r8, #0	; k = 0
 	B loop2
+; ------------------------------------------------------------------------------------------------
 
 loop3
 	CMP r8, r5	; k < source1->columns <=> k < source2->rows
 	BXEQ LR
-	MLA r9, r6, r5, r8	; mul_data1_address = i * col1 + j (i * r5 + j)
-	MLA r10, r8, r4, r7	; mul_data2_address = j * col2 + k (j * r4 + k)
+	MLA r9, r6, r5, r8	; mul_data1_address = i * col1 + k (i * r5 + k)
+	MLA r10, r8, r4, r7	; mul_data2_address = k * col2 + j (k * r4 + j)
 	LSL r9, r9, #2	; mul_data1_address *= 4
 	LSL r10, r10, #2	; mul_data2_address *= 4
 	ADD r9, r1, r9	; mul_data1_address += source1_data_address
 	ADD r10, r2, r10	; mul_data2_address += source2_data_address
-	LDR r9, [r9]	; mul_data1 = source1[i,j]
-	LDR r10, [r10]	; mul_data2 = source2[j,k]
+	LDR r9, [r9]	; mul_data1 = source1[i,k]
+	LDR r10, [r10]	; mul_data2 = source2[k,j]
 	MUL r9, r9, r10	; mul_data1 *= source2[j,k]
 	MLA r10, r6, r4, r7	; res_data_address = i * col2 + j (i * r4 + j)
 	LSL r10, R10, #2	; res_data_address *= 4
@@ -73,8 +78,10 @@ loop3
 	STR r9, [r10]	; results[i,j] = res_data
 	ADD r8, #1	; k++
 	B loop3
+; ------------------------------------------------------------------------------------------------
 
-return
+finish
+	POP {r3-r4}
 	CMP r3, r4
 	MOVEQ r0, #0 ; success
 	BX LR
