2bd03a1beaae9f5f85bb0833271c4d1fd13a628d
Yusen Zheng <ysyszheng@gmail.com>
2022-05-22 23:15:00 +0800

    create ./lab-2/generate.sh

--- lab-2/matrix_mul_asm.s
+++ lab-2/matrix_mul_asm.s
@@ -1,3 +1,4 @@
+// Author: Yusen Zheng
 
 	.text
 	.global matrix_mul_asm
@@ -5,5 +6,16 @@
 matrix_mul_asm:
     // Please write your code below that will implement:
     //       int matrix_mul_asm(Matrix* results, Matrix* source1, Matrix* source2);
-    
+
+		// Notes:
+		// Data in the matrix is ranging from 0 to 255. 8 bits.
+
+//	LDR X0, [R1]
+//	LDR X1, [R2, #4]
+//	STR X0, [R0]
+//	STR X1, [R0, #4]
+
+	// return 0;
+	MOV		R0, #0
+	
 	RET
